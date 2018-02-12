301f17640b67df75f6e7dea37797f3f5ed0b7a99
Nicolo Paternoster <leg@lize.it>
2018-02-12 12:37:25 +0100

    risolti i problemi con il fullscreen!

--- assets/js/poi.js
+++ assets/js/poi.js
@@ -13,6 +13,7 @@ function toggleScheda(){
 	var schede = document.getElementsByClassName("schedaContenuto");
 	var scheda = schede[0];
 	var vis = scheda.style.visibility;
+
 	if(vis=="visible")
 		scheda.style.visibility="hidden";
 	else 
@@ -25,4 +26,18 @@ function hideScheda(scheda){
 
 function showScheda(scheda){
 	document.getElementsByClassName("schedaContenuto")[0].style.visibility = "visible";
+
 }
+
+function addSchedaToPano(){
+	var krpano = document.getElementById("krpanoSWFObject");
+	var layer = krpano.get("control.layer");
+	layer.appendChild(scheda);
+}
+
+
+//Equivalent of jQuery onDocument ready thanks to https://stackoverflow.com/a/30319853
+r(function(){
+        addSchedaToPano();
+    });
+    function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
