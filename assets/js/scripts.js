87c99bbb2a0276ddea93d5549d2b7809bd2125d5
Nicolo Paternoster <leg@lize.it>
2018-02-12 12:01:03 +0100

    removed fullscreen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,8 +40,7 @@ function initMap() {
 
   
 
-    nocemap.addControl(new L.Control.Fullscreen());
-
+    //nocemap.addControl(new L.Control.Fullscreen()); //Removed fullscreen because in fullscreen mode all divs are hidden otherwise and there is no solution
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 16,
